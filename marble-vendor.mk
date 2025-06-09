d2a7e948173ecda00e91b2c14c172532fcbe900d
Joey Huab <jhuab@edukasyon.ph>
2025-06-09 18:37:12 +0900

    Revert "marble: Drop prebuilt libdisplayconfig.qti"
    This reverts commit fc608c03c694b5991d9c496e84b347d8ac1b0cf3.

--- marble-vendor.mk
+++ marble-vendor.mk
@@ -850,6 +850,7 @@ PRODUCT_PACKAGES += \
     libcom.xiaomi.metadatautils \
     libcom.xiaomi.pluginutils \
     libdeflicker \
+    libdisplayconfig.qti \
     libgf_ca \
     libgf_hal \
     libgoodixhwfingerprint \
